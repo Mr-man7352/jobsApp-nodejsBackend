d830fe163178e57b96bba6296bc019394e475f52
Manish Sahu <manissh7352@gmail.com>
2021-12-08 20:39:47 +0530

    added jwt with moongse methods

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,8 +4,9 @@ const { StatusCodes } = require("http-status-codes");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
+  const token = user.createJWT();
 
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
@@ -23,4 +24,4 @@ module.exports = {
 //   throw new BadRequestError("Please provide name , email and password");
 // }
 
-// adding comment
+// adding commentdsfasf
